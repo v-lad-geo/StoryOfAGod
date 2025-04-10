af03bc1cce622af2dd413ea13437aa7b6f3419f6
v-lad-geo <vladgeorge.pohanka@gmail.com>
2025-04-10 13:14:49 +0300

    -first part of main -gitignore

---
+++ StoryOfAGod/Intermediate/Build/Win64/x64/StoryOfAGodEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "D:/Github-repo/StoryOfAGod/Intermediate/Build/Win64/x64/StoryOfAGodEditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ StoryOfAGod/Intermediate/Build/Win64/x64/StoryOfAGodEditor/Development/CoreUObject/SharedPCH.CoreUObject.RTTI.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
+#include "D:/Github-repo/StoryOfAGod/Intermediate/Build/Win64/x64/StoryOfAGodEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.RTTI.Cpp20.h"
+#include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
