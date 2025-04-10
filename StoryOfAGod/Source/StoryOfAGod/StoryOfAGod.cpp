f8490fd76674ee07671d6e95c3e4c67cf574da88
v-lad-geo <vladgeorge.pohanka@gmail.com>
2025-04-10 13:15:19 +0300

    -second part of main

---
+++ StoryOfAGod/Source/StoryOfAGod/StoryOfAGod.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StoryOfAGod.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StoryOfAGod, "StoryOfAGod" );
